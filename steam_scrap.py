3eddd68b14300a7b5dcc842bea23bddb3c888c50
Unknown <joao88vsc@gmail.com>
2017-10-02 15:16:25 +0100

    refactor

--- steam_scrap.py
+++ steam_scrap.py
@@ -1,24 +1,25 @@
+"""
+File responsable to get information about steam games
+"""
 import requests
 
-def getAppID(appIDListArg,name):
- for i in appIDListArg:
-		if i['name'] == name:
-			gameAppID = i['appid']
-			print(name + " App ID: " + str(gameAppID))
-			return gameAppID
+def getappid(appid_games_list, name):
+    """ Function responsable to get the App ID of a game, given a name"""
+    for i in appid_games_list:
+        if i['name'] == name:
+            print(name + " App ID: " + str(i['appid']))
+            return i['appid']
 
 if __name__ == '__main__':
-	
-	#request responsable to return a json object with all the steam games
-	r = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
+    #request responsable to return a json object with all the steam games
+    r = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
 
-	#store appID and Names of the games into a List
-	appIDList = r.json()['applist']['apps']
-	
-	#get appID given the name of the Game
-	appID_Dota = getAppID(appIDList,'Dota 2')
+    #store appID and Names of the games into a List
+    games_list = r.json()['applist']['apps']
+    #get appID given the name of the Game
+    appid_dota = getappid(games_list, 'Dota 2')
 
-	#with the appID it's possible to use the steam api to do all the get's we need about a game
+    #with the appID it's possible to use the steam api to do all the get's we need about a game
 
-	r_Dota = requests.get('http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=' + str(appID_Dota) + '&format=json')
-	print (r_Dota.json())
+    r_dota = requests.get('http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=' + str(appid_dota) + '&format=json')
+    print (r_dota.json())
