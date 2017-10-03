f2da752bc822933ddd144e7c829bb2fd1bf00f8a
Nuno Silva <ei12187@fe.up.pt>
2017-10-03 12:20:27 +0100

    Refactored Metacritic web scraping, now searches Metacritic for game page

--- metacritic_scrap.py
+++ metacritic_scrap.py
@@ -4,24 +4,41 @@ import csv
 import re
 from bs4 import BeautifulSoup
 
-def metac_scrap(meta_url):
+def metac_scrap(vg_name):
+
+    # replace spaces with unicode char
+    vg_name = vg_name.replace(' ', '%20')
+
+    # search Metacritic for the video game's page
+    search_page = urllib2.Request('http://www.metacritic.com/search/all/' + vg_name + '/results', headers={'User-Agent': 'Mozilla/5.0'})
+
+    # query the website and return the html to the variable 's_page'
+    s_page = urllib2.urlopen(search_page)
+
+    # parse the html using BeautifulSoup and store in variable 's_soup'
+    s_soup = BeautifulSoup(s_page, 'html.parser')
+
+    # grab the Metacritic URL for the video game's page on the domain
+    meta_url_box = s_soup.find('h3', attrs={'class': 'product_title basic_stat'})
+    meta_url = 'http://metacritic.com' + meta_url_box.find('a')['href']
+
     # specify the url
     title_page = urllib2.Request(meta_url, headers={'User-Agent': 'Mozilla/5.0'})
 
-    # query the website and return the html to the variable 'page'
-    page = urllib2.urlopen(title_page)
+    # query the website and return the html to the variable 't_page'
+    t_page = urllib2.urlopen(title_page)
 
-    # parse the html using Beautiful Soup and store in variable 'soup'
-    soup = BeautifulSoup(page, 'html.parser')
+    # parse the html using Beautiful Soup and store in variable 't_soup'
+    t_soup = BeautifulSoup(t_page, 'html.parser')
 
-    # take out the <div> of object to find and get its value
-    name_box = soup.find('span', attrs={'itemprop': 'name'})
+    # take out the <span> of object to find and get its value
+    name_box = t_soup.find('span', attrs={'itemprop': 'name'})
     name = name_box.text.strip()
 
-    score_box = soup.find('span', attrs={'itemprop': 'ratingValue'})
+    score_box = t_soup.find('span', attrs={'itemprop': 'ratingValue'})
     score = score_box.text
 
-    uscore_box = soup.find('div', attrs={'class': re.compile('^metascore_w user')})
+    uscore_box = t_soup.find('div', attrs={'class': re.compile('^metascore_w user')})
     uscore = uscore_box.text
 
     # open a csv file with append, so old date will not be erased
@@ -33,4 +50,4 @@ def metac_scrap(meta_url):
 
 # call the module directly for testing
 if __name__ == "__main__":
-    metac_scrap('')
+    metac_scrap('uncharted 2')
