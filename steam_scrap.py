e77aca1d03640c728e127458f58374b8aa8e98ab
Unknown <joao88vsc@gmail.com>
2017-10-07 15:54:52 +0100

    get the information about the game of the steam website

--- steam_scrap.py
+++ steam_scrap.py
@@ -1,6 +1,13 @@
 """
 File responsable to get information about steam games
 """
+#import the library used to query a website
+import urllib2
+
+#import the Beautiful soup functions to parse the data returned from the website
+from bs4 import BeautifulSoup
+
+#import the request to get the appID
 import requests
 
 def getappid(appid_games_list, name):
@@ -10,16 +17,34 @@ def getappid(appid_games_list, name):
             print(name + " App ID: " + str(i['appid']))
             return i['appid']
 
-if __name__ == '__main__':
+def getgameinfo(urlsteam, appid):
+    
+    pageurl = urllib2.Request(urlsteam + str(appid))
+    #Query the website and return the html to the variable 'page'
+    page = urllib2.urlopen(pageurl)
+    #Parse the html in the 'page' variable, and store it in Beautiful Soup format
+    soup = BeautifulSoup(page, "lxml")
+
+    reviews = soup.findAll('span', class_='nonresponsive_hidden responsive_reviewdesc')
+
+    game_description = soup.find('div', class_='game_description_snippet')
+
+    game_price = soup.find('div', class_='game_purchase_price price')
+
+    #possibility to recomended games (NOT FINISHED)
+    games_recomended = soup.find_all('a', class_="small_cap")
+    print([reviews,game_description.string,game_price.string,games_recomended])
+
+if __name__ == '__main__':    
     #request responsable to return a json object with all the steam games
     r = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
 
     #store appID and Names of the games into a List
-    games_list = r.json()['applist']['apps']
+    gameslist = r.json()['applist']['apps']
     #get appID given the name of the Game
-    appid_dota = getappid(games_list, 'Dota 2')
+    appiddota = getappid(gameslist, 'Dota 2')
 
-    #with the appID it's possible to use the steam api to do all the get's we need about a game
-
-    r_dota = requests.get('http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=' + str(appid_dota) + '&format=json')
-    print (r_dota.json())
+    #specify steam url
+    url = "http://store.steampowered.com/app/"
+    
+    getgameinfo(url, appiddota)
